9492991c8585ae8b2c1a98e0726ba20d8dd59089
Sorachat <73382296+ter2002@users.noreply.github.com>
2021-11-17 01:22:45 +0700

    index01

--- components/TextTitle.jsx
+++ components/TextTitle.jsx
@@ -6,13 +6,13 @@ const StyledTitle = styled.p`
   font-family: 'Kanit', sans-serif;
   color: ${props => props.color};
   margin: 0;
-  width: 60%;
+  width: ${props => props.width ? props.width : "60%"};
   line-height: 1.3;
 `
 
-const Title = ({ textColor, children, fontSize,textAlign }) => {
+const Title = ({ textColor, children, fontSize,textAlign,width }) => {
     return (
-        <StyledTitle color={textColor} fontSize={fontSize} textAlign={textAlign}>
+        <StyledTitle color={textColor} fontSize={fontSize} textAlign={textAlign} width={width}>
             {children}
         </StyledTitle>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,8 @@ import { Head, NavBar, Footer, Button, Title } from "../components";
 import styled, { css, keyframes } from 'styled-components'
 import {Card, BlankCard, NewsCard, TextBG, NewsFrame} from '../components/NewsComponents'
 import styles from '../styles/News.module.css'
+import logo from '../public/img/video_1.jpg'
+import {useState} from 'react'
 // import sample from './sample.mp4';
 const TitleUp = keyframes`
     from {
@@ -58,10 +60,33 @@ const Cen2 = styled.div`
     
     }
 `
+const Cen3 = styled.div`
+    position:absolute;
+    top: 190px;
+    left: 60%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 100%;
+    
+    
+
+    
+    }
+`
 const Content = styled.div`
     background-color:#ebe8e1;
     
     
+
+    
+    }
+`
+const Content03 = styled.div`
+    background-color:#ebe8e1;
+    position:relative;
+    ${'' /* width:100vw;
+    height:100vh; */}
+    display: block;
 
     
     }
@@ -74,6 +99,8 @@ const BackgroundText = styled.div`
     right: 0;
     z-index: 0;
     overflow: hidden;
+  
+    
     
     
 
@@ -85,11 +112,100 @@ const Container = styled.div`
     position: relative;
     margin-left:144px;
     margin-top:170px;
-
+    
     // z-index:2000;
 `
 
+const Text01 = styled.div`
+    width:19vw;
+    opacity:0.9;
+    position: relative;
+    margin-left:144px;
+    margin-top:100px;
+
+    // z-index:2000;
+`
+const Content01 = styled.div`
+    display:flex;
+    
+`
+const Content02 = styled.div`
+     background-color:#f84754;
+     width:100vw;
+     height:auto;
+     
+     ${'' /* margin-bottom:2000px; */}
+`
+const Video01 = styled.div`
+    width:19vw;
+    opacity:1;
+    position: relative;
+    margin-left:144px;
+    margin-top:100px;
+    // z-index:2000;
+`
+const Svideo = styled.div`
+    position:fixed;
+    width:100vw;
+    height:100vh;
+    display:${props=>props.show?'flex':'none'};
+    z-index:15000;
+    flex-direction: column;
+    top: 0;
+    
+`
+const ExampleText =styled.p`
+    &:hover {
+        color:black;
+    }
+    color:#f84754;
+    cursor:pointer;
+    transition: all .1s ease-in-out;
+`
+const ContentBG1 =styled.div`
+    background-image: url("https://i.ytimg.com/vi/PcpJPTodvgA/maxresdefault.jpg");
+    width:100vw;
+    height:100vh;
+
+`
+const ContentBG2 =styled.div`
+    position:relative;
+    background-image: url("https://media.discordapp.net/attachments/896040304002617374/910186033667264572/maps-03ebbf2c074f13a65af1dba0c80f767e.png?width=959&height=670");
+    background-size:cover;
+    ${'' /* right:0; */}
+    width:80vw;
+    height:100vh;
+    bottom:100px;
+    left:400px;
+    background-repeat:none;
+
+`
+const Text02 =styled.div`
+    color:white;
+    width:100%;
+    height:100vh;
+    position:absolute;
+
+`
+const Text_all =styled.div`
+    ${'' /* background-color:red; */}
+    color:white;
+    width:30vw;
+    height:100vh;
+    position:absolute;
+    right:10vw;
+
+`
+const Margin = styled.div`
+    padding-top:100px;
+`
+
+
+
+
+
 export default function Home() {
+    const [showVideo, setShowVideo] = useState(false); 
     return (
         <>
             <Head title="VALORANT: เกมยิงปืนจาก Riot Games ในรูปแบบ 5v5 ที่ขับเคลื่อนโดยตัวละครนักยิงปืนผู้มากความสามารถ" />
@@ -111,17 +227,110 @@ export default function Home() {
                 </Section>
                 <Container>
                     <BackgroundText>
-                        <h2 className={ styles.stroke } style={{opacity:0.1}} >
-                            {/* <TextBG className={ styles.textBackground1 }>WE ARE</TextBG>
-                            <TextBG className={ styles.textBackground2 }>Valona</TextBG> */}
+                        <h2 className={ styles.stroke } style={{opacity:0.2}} >
+                            <p  style={{
+                                fontSize:"20rem",
+                                position:"absolute",
+                                top:"-344px"
+                               
+                                }}>WE ARE</p>
+                            <p style={{
+                                fontSize:"20rem",
+                                position:"absolute",
+                                top:"-80px",
+                                
+                                right:"150px"
+                               
+                                }}>VALORA</p>
+                            
                         </h2>
-                    </BackgroundText>
+                    </BackgroundText >
                     <StyleTitle textColor="#f84754" fontSize="6.5rem">ข่าวสารล่าสุด</StyleTitle>
-                    <hr style={{ width:"50vw" }}></hr>
+                    <hr style={{ width:"70vw" ,position:"absolute",left:"10%",margin:"0"}}></hr>
+                    <Cen3>
                     <Title fontSize="5.6rem">พวกเราคือ VALORANT</Title>
+                    </Cen3>
+                    
+                    <Content01>
+                    <Text01>
+                        <h3>ก้าวข้ามทุกขีดจำกัด</h3>
+                        <br></br>
+                        <p>ผสมผสานสไตล์และประสบการณ์ของคุณบนเวทีการแข่งขันระดับโลก คุณมีโอกาสในการเล่น 13 รอบเพื่อโจมตีและป้องกันทีมของคุณด้วยการยิงที่เฉียบคมและความสามารถทางยุทธวิธี และคุณจะต้องตัดสินใจให้เร็วกว่าคู่ต่อสู้หากคุณต้องการเอาชีวิตรอดด้วยชีวิตเดียวที่มีอยู่ สังหารศัตรูในโหมดแข่งขันและโหมดไม่จัดอันดับ เช่นเดียวกันกับโหมด Deathmatch และ Spike Rush</p>
+                        <br></br>
+                        <ExampleText  onClick={()=>setShowVideo(true)}>ดูตัวอย่างเกมเพลย์ที่นี่</ExampleText>
+                    </Text01>
+                   
+                    <Video01>
+                    
+                    <img style={{width:"47vw", height:"", opacity:"1",cursor:"pointer",marginBottom:"150px"}} onClick={()=>setShowVideo(true)} src="/project/g41playvalorant/img/video_1.jpg" alt="Logo" />
+                    </Video01>
+                    
+                    </Content01>
                 </Container>
             </Content>
+            <Svideo show={showVideo}>
+                        <button onClick={()=>setShowVideo(false)}>close</button>
+                        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/6A_XP5NynCQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Svideo>
+            <Content02 style={{paddingTop:"50px"}}>
+            <Text_all>
+            <hr style={{opacity:"0.3"}}></hr>
+            <Title fontSize="5rem" width="100%">เอเจนท์ของคุณ</Title>
+                <Text02>
+                    <br></br>
+                    <h4>ความคิดสร้างสรรค์คืออาวุธที่แข็งแกร่งที่สุดของคุณ</h4>
+                    <br></br>
+                    <p>เกมนี้ไม่ได้มีเพียงแค่ปืนหรือกระสุน คุณจะได้เลือกเอเจนท์ที่มาพร้อมอาวุธและความสามารถที่ยืดหยุ่น รวดเร็ว และรุนแรง ซึ่งความสามารถเหล่านี้จะช่วยฉายแสงให้ทักษะการยิงปืนของคุณเปล่งประกายยิ่งขึ้น ไม่มีเอเจนท์ไหนที่มีสไตล์การเล่นที่เหมือนกัน เช่นเดียวกับการที่ไม่มีไฮไลท์ใดที่เหมือนกัน</p>
+                    <br></br>
+                    <Button isWhite style={{}}>ดูเอเจนท์ทั้งหมด</Button>
+                </Text02>
+                </Text_all>
+                
+                <ContentBG1></ContentBG1>
+                
+            </Content02>
+            <Content03>
+            <Margin>
+            <BackgroundText style={{
+                top:"px",
+            }}>
+                        <h2 className={ styles.stroke } style={{opacity:0.2}} >
+                            <p  style={{
+                                fontSize:"20rem",
+                                position:"absolute",
+                               margin:0
+                                ,left:"150px"
+                               
+                                }}>PLACE</p>
+                            
+                            
+                        </h2>
+                    </BackgroundText>
+            <Text_all style={{
+                zIndex:"1",
+                left:"200px",
+                top:"100px",
+                color:"black"
+            }}>
+            <hr></hr>
+                <Title fontSize="6rem" width="100%">แผนที่ของคุณ</Title>
+                <Text02 style={{
+                
+                color:"black"
+            }}>
+                    <br></br>
+                    <h4 style={{opacity:"0.8"}}>ต่อสู้รอบโลก</h4>
+                    <br></br>
+                    <p style={{opacity:"0.5"}}>แผนที่แต่ละอันคือพื้นที่จัดแสดงความคิดสร้างสรรค์ของคุณ แผนการของทีมที่มีจุดมุ่งหมาย การเล่นที่น่าตื่นเต้น และช่วงเวลาที่น่ากังวล เล่นให้เป็นตำนานจนคนอื่นต้องตามรอย</p>
+                    <br></br>
+                    <Button>แสดงแผนที่ทั้งหมด</Button>
+                </Text02>
+            </Text_all>
+                <ContentBG2></ContentBG2>
+                </Margin>
+            </Content03>
             <Footer />
+            
         </>
     )
 }
